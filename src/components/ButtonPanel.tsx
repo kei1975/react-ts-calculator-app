bc63cc35d1d334d35d4969ee2fef55344ab7abde
Okumura Kei <kei@iMac5K2017.local>
2022-08-09 12:27:56 +0900

    modified:adding display and buttons

---
+++ src/components/ButtonPanel.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default function ButtonPanel() {
+  return (
+    <div>
+      <div>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>AC</button>
+      </div>
+       <div>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>-</button>
+      </div>
+       <div>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>+</button>
+      </div>
+       <div>
+        <button>0</button>
+        <button>.</button>
+        <button>D</button>
+        <button>=</button>
+      </div>
+    </div>
+  )
+}
--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import ButtonPanel from './ButtonPanel'
+import Display from './Display'
 
 export default function Calculator() {
   return (
     <div>
-        Hello
+        <Display />
+        <ButtonPanel />
     </div>
   )
 }
